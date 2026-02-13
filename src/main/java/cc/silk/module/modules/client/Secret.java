51d6417fe31dc16f5c8d10ccd70d3361ab83bc07
nguyenphuc2407 <138024068+nguyenphuc2407@users.noreply.github.com>
2026-02-13 08:37:47 +0700

    Update constructor description for Secret module

--- src/main/java/cc/silk/module/modules/client/Secret.java
+++ src/main/java/cc/silk/module/modules/client/Secret.java
@@ -6,6 +6,6 @@ import cc.silk.module.Module;
 public class Secret extends Module {
 
     public Secret() {
-        super("Phuc Nguyen", "Nguyen Dep Trai Vl Tiktok npnchill", 0, Category.CLIENT);
+        super("Phuc Nguyen", "TikTok: npnchill", 0, Category.CLIENT);
     }
 }
