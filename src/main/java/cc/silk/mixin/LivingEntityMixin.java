ba11a2d217184f4b79c8c47274f97427614bfded
nguyenphuc2407 <138024068+nguyenphuc2407@users.noreply.github.com>
2026-02-13 11:42:32 +0700

    Update LivingEntityMixin.java

--- src/main/java/cc/silk/mixin/LivingEntityMixin.java
+++ src/main/java/cc/silk/mixin/LivingEntityMixin.java
@@ -1,24 +1,12 @@
- package cc.silk.mixin;
-import cc.silk.SilkClient;
+package cc.silk.mixin;
+
 import net.minecraft.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public class LivingEntityMixin {
 
-    @Inject(method = "getHandSwingDuration", at = @At("HEAD"), cancellable = true)
-    public void getHandSwingDurationInject(CallbackInfoReturnable<Integer> cir) {
-        if (SilkClient.INSTANCE == null || SilkClient.mc == null) return;
-
-        var optionalModule = SilkClient.INSTANCE.getModuleManager().getModule(SwingSpeed.class);
-        if (optionalModule.isPresent()) {
-            SwingSpeed module = optionalModule.get();
-            if (module.isEnabled()) {
-                cir.setReturnValue(module.getSwingSpeed());
-            }
-        }
-    }
+    // Đã xoá toàn bộ SwingSpeed hook.
+    // Không inject vào getHandSwingDuration nữa.
+    // Mixin trống để tránh lỗi compile và không gây crash.
 }
